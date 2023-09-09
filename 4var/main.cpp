ab702370f156b425cd293e663602419ce56d2ff3
Polina Gonokhova <107886348+lunti4@users.noreply.github.com>
2023-09-09 11:57:56 +0700

    Update main.cpp

--- 4var/main.cpp
+++ 4var/main.cpp
@@ -14,26 +14,30 @@ struct bankClient {         /// структура в которой все хр
 
 };
 
-void split(string const &str, const char delitel, vector<string> &result) {     /// функция разделения строки по делителю, результат помещаем в вектор резал(которые передаем)
-    
+vector<string> split(string& str, char delitel) {     /// функция разделения строки по делителю, результат помещаем в вектор резал(которые передаем)
+
+    vector<string> results;
+
     stringstream ss(str);              /// создаем ss stringline (поток строк) для работы с гетлайном
 
     string s;                           /// строка куда будем помещать строки, на которые поделили
 
-    while(getline(ss, s, delitel)) {       /// пока делится ss в s по delitel, записываем результат в s и эту строку s добавляем ко всем остальным в вектор result
-        result.push_back(s);
+    while (getline(ss, s, delitel)) {       /// пока делится ss в s по delitel, записываем результат в s и эту строку s добавляем ко всем остальным в вектор result
+        results.push_back(s);
     }
+
+    return results;
 
 }
 
-void alphabeticOrderPrint(vector <bankClient> &clients) {           /// функция печати в алфавитном порядке, которая принимает на вход вектор с клиентами
+void alphabeticOrderPrint(vector <bankClient>& clients) {           /// функция печати в алфавитном порядке, которая принимает на вход вектор с клиентами
 
     vector<string> firstNames;                      /// сюда положим фамилии
-    
 
-    for (int i = 0; i < clients.size(); i++) {      
-        vector<string> tmp;         
-        split(clients[i].fio, ' ', tmp);            /// прооходимся по каждому фио и делим его на [ф, и, о]
+
+    for (int i = 0; i < clients.size(); i++) {
+        vector<string> tmp;
+        tmp = split(clients[i].fio, ' ');            /// прооходимся по каждому фио и делим его на [ф, и, о]
 
 
         firstNames.push_back(tmp[0]);               /// кладем фамилию в вектор фамилий
@@ -44,25 +48,25 @@ void alphabeticOrderPrint(vector <bankClient> &clients) {           /// функ
     cout << "Количество человек - " << firstNames.size() << '\n';
 
     for (int i = 0; i < firstNames.size(); i++) {
-        cout << i+1 << ". " << firstNames[i] << "\n";
+        cout << i + 1 << ". " << firstNames[i] << "\n";
     }
 
 }
 
 
-void getResult(vector<bankClient> &clients, string &date, int balance) {        /// основная рабочая функция, принимает клиентов которых мы положили в вектор, строку с датой и баланс который мы ввели
+void getResult(vector<bankClient>& clients, string& date, int balance) {        /// основная рабочая функция, принимает клиентов которых мы положили в вектор, строку с датой и баланс который мы ввели
 
     vector<bankClient> validClients;                /// вектор, где будут храниться клиенты, которые нам подходят по условию
 
-    for(int i=0; i < clients.size(); i++) {
-        
+    for (int i = 0; i < clients.size(); i++) {
+
         vector<string> tmp;
-        split(clients[i].changeDate, '.', tmp);     /// из поля changedate мы хотим получить месяц, поэтому бьем строку
+        tmp = split(clients[i].changeDate, '.');     /// из поля changedate мы хотим получить месяц, поэтому бьем строку
 
         string clientDate = tmp[1];                 /// клиентская дата = месяц, которые мы получили
 
         if ((clientDate == date) & (balance < clients[i].balance)) {        /// если дата клиента == дата введенная и введенный баланс < баланс клиента => клиент валидный
-            validClients.push_back(clients[i]);
+            validClients.push_back(clients[i]);                               /// добавляем в массив
         }
     }
 
@@ -74,45 +78,45 @@ void getResult(vector<bankClient> &clients, string &date, int balance) {        
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    ifstream file;                     
+    ifstream file;
     file.open("test.txt");
-    
+
     vector<bankClient> clients;                     /// здесь у нас хранятся все клиенты
 
-    string str;
-    
+    string str;                                       /// сюда помещаем строки из файла
+
     while (getline(file, str)) {                    /// пока можем получить строку из файла, пишем ее в стр
 
         if (str.size() > 0) {                       /// если строка не пустая, то делим ее по запятой и создаем экземпляр структуры
-            vector<string> tmp;            
-            split(str, ',', tmp);
+            vector<string> tmp;
+            tmp = split(str, ',');
 
             bankClient client;
 
-            client.fio = tmp[0];                    /// устанавливаем поля, после чего добавляем ко всем остальным клиентам
+            client.fio = tmp[0];                    /// задаём поля, после чего добавляем экземпляр в массив
             client.accountNumber = tmp[1];
             client.balance = stoi(tmp[2]);
             client.changeDate = tmp[3];
 
             clients.push_back(client);
-    
+
         }
     }
 
     string date;
     int balance;
 
-    cout << "Введите интересующий месяц в формате XX \n";       /// вводим нужные даты и балансы
+    cout << "Введите интересующий месяц в формате XX и нажмите Enter \n";       /// вводим нужные даты и балансы
     cin >> date;
 
     if ((stoi(date) > 12) | (date.size() != 2)) {
-        
-        cout << "Введен неверный формат даты \n"; 
+
+        cout << "Введен неверный формат даты \n";
         return 0;
 
     }
 
-    cout << "Введите минимальный баланс \n";
+    cout << "Введите минимальный баланс и нажмите Enter \n";
     cin >> balance;
 
     getResult(clients, date, balance);                      /// имея клиентов и балансы вызываем функцию получения результатов
